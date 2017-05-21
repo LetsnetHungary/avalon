106a9b3c06eb3fc8acd522b130d6ce87ab70c266
ADAMS-PC\balas <balassaadi@gmail.com>
2017-05-21 16:21:54 +0200

    suits

--- rules.php
+++ rules.php
@@ -5,9 +5,9 @@
   	<meta name="viewport" content="width=device-width, initial-scale=1">
      <link rel="shortcut icon" href="favicon.ico" />
 		<!--<link href="https://fonts.googleapis.com/css?family=Roboto:300" rel="stylesheet">-->
-		<link rel="stylesheet" type = "text/css" href="../_assets/css/n.css">
-    <link rel="stylesheet" type = "text/css" href="../_assets/css/rules.css">
-    <link rel="stylesheet" type = "text/css" href="../_assets/css/header.css">
+		<link rel="stylesheet" type = "text/css" href="../linkafestmenyei/_assets/css/n.css">
+    <link rel="stylesheet" type = "text/css" href="../linkafestmenyei/_assets/css/rules.css">
+    <link rel="stylesheet" type = "text/css" href="../linkafestmenyei/_assets/css/header.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <meta charset="utf-8">
     <title>Avalon</title>
